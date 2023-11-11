e667ad73ddcd52a2c5fcad499f3938622121b3c8
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-11-11 11:49:27 +0400

    Add project files.

---
+++ AttributeTest1/ColorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeTest1
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+	internal class ColorAttribute : Attribute
+	{
+		public ConsoleColor Color;
+
+		public ColorAttribute(ConsoleColor color)
+		{
+			Color = color;
+		}
+	}
+}
---
+++ AttributeTest1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace AttributeTest1
+{
+	[Color(ConsoleColor.Blue)]
+	public class ColorClass
+	{
+		override public string ToString()
+		{
+			return "ColorClass";
+		}
+	}
+
+	internal class Program
+	{
+		static void ShowColorClass(ColorClass colorClass)
+		{
+			// 1. Read the attribute of colorClass instance
+			// 2. Based on value, change the color of console
+
+			ColorAttribute? colorAtrb = Attribute.GetCustomAttribute(colorClass.GetType(), typeof(ColorAttribute)) as
+				ColorAttribute;
+
+			if (colorAtrb != null)
+			{
+				ConsoleColor consoleColor = colorAtrb.Color;
+				Console.ForegroundColor = consoleColor;
+			}
+
+			Console.WriteLine(colorClass);
+			Console.ResetColor();
+		}
+
+		static void Main(string[] args)
+		{
+			ShowColorClass(new ColorClass());
+		}
+	}
+}
